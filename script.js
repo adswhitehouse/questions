91e980405d2084583fa340d518bcda1d276bc8a8
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-02-16 22:09:57 +0000

    refined js event listener

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 let expandButtons = document.querySelectorAll(".jsBtn");
 
-let isClicked = false;
 expandButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    if (!isClicked) {
-      button.parentElement.nextElementSibling.classList.add(
-        "question-text-show"
-      );
-      button.innerHTML = `<i class="fa-solid fa-minus"></i>`;
-      isClicked = true;
-    } else {
-      button.parentElement.nextElementSibling.classList.remove(
-        "question-text-show"
-      );
-      button.innerHTML = `<i class="fa-solid fa-plus"></i>`;
-      isClicked = false;
-    }
+    button.parentElement.nextElementSibling.classList.toggle(
+      "question-text-show"
+    );
+    button.lastElementChild.classList.toggle("hidden");
+    button.firstElementChild.classList.toggle("hidden");
   });
 });
